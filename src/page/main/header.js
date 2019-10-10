493bbaa0552c5114847517df1d1d86e5fab785d8
ckdtjs505 <ckdtjs505@naver.com>
2019-10-10 23:04:05 +0900

    [오류해결] route 페이지 경로 해결

--- src/page/main/header.js
+++ src/page/main/header.js
@@ -3,6 +3,7 @@ import {
   Navbar,
   NavbarBrand,
  } from 'reactstrap';
+ import { Link } from 'react-router-dom';
 
 export default class Example extends React.Component {
   constructor(props) {
@@ -22,7 +23,7 @@ export default class Example extends React.Component {
     return (
       <div style={{ maxWidth : "100%", backgroundColor : "black"}}>
         <Navbar style={{maxWidth : "90%", margin : "auto", padding : "0"}} dark expand="md">
-          <NavbarBrand href="/">New space</NavbarBrand>
+          <NavbarBrand  tag={Link} to={process.env.PUBLIC_URL + "/"}>New space</NavbarBrand>
         </Navbar>
       </div>
     );
--- src/page/simulation/header.js
+++ src/page/simulation/header.js
@@ -7,17 +7,17 @@ import {
   Nav,
   NavItem,
   NavLink } from 'reactstrap';
-
+import { Link } from 'react-router-dom';
 export default class Example extends React.Component {
   constructor(props) {
     super(props);
-    console.log(process.env.PUBLIC_URL);
+    
     this.toggle = this.toggle.bind(this);
     this.state = {
-      lin : process.env.PUBLIC_URL + '/',
       isOpen: false
     };
   }
+  
   toggle() {
     this.setState({
       isOpen: !this.state.isOpen
@@ -28,18 +28,18 @@ export default class Example extends React.Component {
     return (
       <div style={{ maxWidth : "100%", backgroundColor : "black"}}>
         <Navbar style={{maxWidth : "90%", margin : "auto", padding : "0"}} dark expand="md">
-          <NavbarBrand href= {this.state.lin} >New space</NavbarBrand>
+          <NavbarBrand  tag={Link} to={process.env.PUBLIC_URL +'/'}  >New space</NavbarBrand>
           <NavbarToggler onClick={this.toggle} />
           <Collapse isOpen={this.state.isOpen} navbar>
             <Nav className="ml-auto" navbar>
               <NavItem>
-                <NavLink style={{width : "10rem", color : "white", textAlign: "center" , backgroundColor : "red"}} href={this.state.lin + "simulation/"}>우주 시뮬레이션</NavLink>
+                <NavLink tag={Link} to={process.env.PUBLIC_URL + "/simulation"} style={{width : "10rem", color : "white", textAlign: "center" , backgroundColor : "red"}} href={this.state.lin + "simulation/"}>우주 시뮬레이션</NavLink>
               </NavItem>
               <NavItem>
-                <NavLink style={{width : "10rem",textAlign: "center" }} href={this.state.lin + "search/"}>우주 검색</NavLink>
+                <NavLink tag={Link} to={process.env.PUBLIC_URL + "/search"} style={{width : "10rem",textAlign: "center" }} >우주 검색</NavLink>
               </NavItem>
               <NavItem>
-                <NavLink style={{width : "10rem", textAlign: "center"}} href="/components/">신우주항공</NavLink>
+                <NavLink tag={Link} to={process.env.PUBLIC_URL + "/search"} style={{width : "10rem", textAlign: "center"}} >신우주항공</NavLink>
               </NavItem>
             
             </Nav>

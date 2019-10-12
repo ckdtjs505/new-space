9e126c93189f2fe93ad55c93bc3fa469c9b83fc9
ckdtjs505 <ckdtjs505@naver.com>
2019-10-12 18:58:01 +0900

    [UPDATE] ICON 추가, MAIN TAP 완성

--- src/page/main/newspace.js
+++ src/page/main/newspace.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import './style.css';
+import {IoIosRocket, GiEarthAmerica, IoIosSearch} from 'react-icons/all'
 
 function Newspace () {
   return ( 
@@ -10,9 +11,27 @@ function Newspace () {
           <h2>New space는 우주에 관련된 정보를 제공합니다</h2>
       </div>
       <div className="tap">
-          <Link to = {"/simulation"} className="tap_content" ><h3>우주 시뮬레이션</h3></Link>
-          <Link to = {"/search"} className="tap_content" ><h3>우주 검색</h3></Link>
-          <Link to = {"/search"} className="tap_content" ><h3>신우주항공</h3></Link>
+          <Link to = {"/simulation"} className="tap_content" >
+            <div>
+              <h3 className="tap_title">우주 시뮬레이션</h3>
+               <GiEarthAmerica className="icon"></GiEarthAmerica>
+              <h4 className="tap_subtitle">우주 경험 <br/> 하러가기</h4>
+            </div>
+          </Link>
+          <Link to = {"/search"} className="tap_content" >
+            <div>
+              <h3 className="tap_title">우주 검색</h3>
+               <IoIosSearch className="icon"></IoIosSearch>
+              <h4 className="tap_subtitle"> 우주 이미지 <br/> 무료로 다운로드</h4>
+            </div>
+          </Link>
+          <Link to = {"/search"} className="tap_content" >
+          <div>
+              <h3 className="tap_title">신우주항공</h3>
+               <IoIosRocket className="icon"></IoIosRocket>
+              <h4 className="tap_subtitle">우주란 <br/> 무엇일까?</h4>
+            </div>
+          </Link>
       </div>
     </div>
   );

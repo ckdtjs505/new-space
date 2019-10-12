2f887a5082f0b731109c8d22d1a75b1f947d8a5d
ckdtjs505 <ckdtjs505@naver.com>
2019-10-12 10:24:13 +0900

    [리펙터링] MAIN CSS 분리

--- src/page/main/main.js
+++ src/page/main/main.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-import BackgroundImg from '../../back.jpg';
+import './style.css';
 
 function Main () {
   return ( 
-    <div style={{  height : '972px',  backgroundPosition: "center", background : `url(${BackgroundImg})` , backgroundSize : "cover" }}>
-      <div  style = {{ paddingTop : "8rem", marign : "auto"}}>
-          <h1 style ={{ textAlign : "center", color : "white"}}>신우주항공을 탐험하다</h1>
-          <h2 style ={{ textAlign : "center", color : "white"}}>New space는 우주에 관련된 정보를 제공합니다</h2>
+    <div className="main">
+      <div  className = "title">
+          <h1>신우주항공을 탐험하다</h1>
+          <h2>New space는 우주에 관련된 정보를 제공합니다</h2>
       </div>
-      <div style = {{paddingTop : "5rem",  width : "55rem", margin : "auto", display : "flex", justifyContent : "space-between"}}>
-          <Link to = {"/simulation"} style = {{width : "250px", height : "400px", backgroundColor : "rgba(255,255,225, 0.7)", border : "3px solid white"}}></Link>
-          <Link to = {"/search"} style = {{width : "250px", height : "400px", backgroundColor : "rgba(255,255,225, 0.7)", border : "3px solid white"}}></Link>
-          <Link to = {"/search"} style = {{width : "250px", height : "400px", backgroundColor : "rgba(255,255,225, 0.7)", border : "3px solid white"}}></Link>
+      <div className="tap">
+          <Link to = {"/simulation"} className="tap_content" ><h3>우주 시뮬레이션</h3></Link>
+          <Link to = {"/search"} className="tap_content" ><h3>우주 검색</h3></Link>
+          <Link to = {"/search"} className="tap_content" ><h3>신우주항공</h3></Link>
       </div>
     </div>
   );

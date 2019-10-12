d24267f49bbaeeddaa0cbf0cd5845ad4e2c2e790
ckdtjs505 <ckdtjs505@naver.com>
2019-10-12 10:05:27 +0900

    [리펙터링] SIMULATION 함수형 컴포넌트 변환

--- src/page/simulation/simulation.js
+++ src/page/simulation/simulation.js
@@ -1,15 +1,11 @@
 import React from 'react';
 
-// import style from './style.css';
-
-class Simulation extends React.Component {
-	render() {
-		return (
-			<div style ={{textAlign : "center", backgroundColor : "black"}}> 
-				<iframe src="https://www.solarsystemscope.com/iframe" width="1000" height="900" title="우주 시뮬레이션" style= {{minWidth: "500px" ,minHeight: "400px", border: "2px solid black"}}></iframe>
-			</div>
-		);
-	}
+function Simulation(){
+	return (
+		<div style ={{textAlign : "center", backgroundColor : "black"}}> 
+			<iframe src="https://www.solarsystemscope.com/iframe" width="1000" height="900" title="우주 시뮬레이션" style= {{minWidth: "500px" ,minHeight: "400px", border: "2px solid black"}}></iframe>
+		</div>
+	);
 }
 
 export default Simulation;

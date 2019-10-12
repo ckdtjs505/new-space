b66c27fbb43c09870b7dd6355a285830bc815b65
ckdtjs505 <ckdtjs505@naver.com>
2019-10-12 09:59:54 +0900

    [리펙터링] HEADER 불필요한 코드 제거 및 함수형 컴포넌트 변환

--- src/page/main/header.js
+++ src/page/main/header.js
@@ -5,27 +5,14 @@ import {
  } from 'reactstrap';
  import { Link } from 'react-router-dom';
 
-export default class Example extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.toggle = this.toggle.bind(this);
-    this.state = {
-      isOpen: false
-    };
-  }
-  toggle() {
-    this.setState({
-      isOpen: !this.state.isOpen
-    });
-  }
-  render() {
-    return (
-      <div style={{ maxWidth : "100%", backgroundColor : "black"}}>
-        <Navbar style={{maxWidth : "90%", margin : "auto", padding : "0"}} dark expand="md">
-          <NavbarBrand  tag={Link} to={"/"}>New space</NavbarBrand>
-        </Navbar>
-      </div>
-    );
-  }
+function Header() {
+  return (
+    <div style={{ maxWidth : "100%", backgroundColor : "black"}}>
+      <Navbar style={{maxWidth : "90%", margin : "auto", padding : "0"}} dark expand="md">
+        <NavbarBrand  tag={Link} to={"/"}>New space</NavbarBrand>
+      </Navbar>
+    </div>
+  );
 }
+
+export default Header;

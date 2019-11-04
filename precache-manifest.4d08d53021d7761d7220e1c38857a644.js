33eb887d88157ea0deadd4d217ec8d97e6b9dce2
ckdtjs505 <ckdtjs505@naver.com>
2019-11-04 23:19:58 +0900

    Updates

---
+++ precache-manifest.4d08d53021d7761d7220e1c38857a644.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d337a3de57b6b6bb51b3ba6f1d03d014",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1d98dcb2c22ce07a21c9",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "a32610ca15c8718cb880",
+    "url": "/static/css/main.07942c0d.chunk.css"
+  },
+  {
+    "revision": "1d98dcb2c22ce07a21c9",
+    "url": "/static/js/2.44d6eb9f.chunk.js"
+  },
+  {
+    "revision": "a32610ca15c8718cb880",
+    "url": "/static/js/main.892e7e98.chunk.js"
+  },
+  {
+    "revision": "80aa3b6163cafafe0f40",
+    "url": "/static/js/runtime-main.0f3d93cf.js"
+  },
+  {
+    "revision": "621cf67a07b404634cf35d0cea2ce41c",
+    "url": "/static/media/back.621cf67a.jpg"
+  },
+  {
+    "revision": "bd52dc24002f0210c9bf61ad82c389c8",
+    "url": "/static/media/universe.bd52dc24.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.97668f1943f6218fcf33901927c91538.js"
+  "/precache-manifest.4d08d53021d7761d7220e1c38857a644.js"
 );
 
 self.addEventListener('message', (event) => {

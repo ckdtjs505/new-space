b2914e2703d5c93e2bfa7a726decd2d325c13700
ckdtjs505 <ckdtjs505@naver.com>
2019-12-07 13:17:46 +0900

    [UPDATE] simulation frame height 지정

--- src/page/simulation/simulation.js
+++ src/page/simulation/simulation.js
@@ -1,12 +1,18 @@
-import React from 'react';
-import './style.css';
+import React from "react";
+import "./style.css";
 
-function Simulation(){
-	return (
-		<div className = "simulation_main" > 
-			<iframe src="https://www.solarsystemscope.com/iframe" width="100%" height="885" title="우주 시뮬레이션" style= {{ border: "0.2px solid black"}}></iframe>
-		</div>
-	);
+function Simulation() {
+  return (
+    <div className="simulation_main">
+      <iframe
+        src="https://www.solarsystemscope.com/iframe"
+        width="100%"
+        height="100%"
+        title="우주 시뮬레이션"
+        style={{ border: "0.2px solid black" }}
+      ></iframe>
+    </div>
+  );
 }
 
 export default Simulation;
